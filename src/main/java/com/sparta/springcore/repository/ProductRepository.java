cab16eb2d44bcce3a2fde41854fcd9b68c4d1e14
leeseungjun <lsj2867@gmail.com>
2021-10-01 21:19:27 +0900

    스프링 심화 4주차

--- src/main/java/com/sparta/springcore/repository/ProductRepository.java
+++ src/main/java/com/sparta/springcore/repository/ProductRepository.java
@@ -1,6 +1,13 @@
 package com.sparta.springcore.repository;
-
 import com.sparta.springcore.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> { }
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findAllByUserId(Long userId, Pageable pageable);
+    List<Product> findAllByUserId(Long userId);
+    Page<Product> findAllByUserIdAndFolderList_Id(Long userId, Long folderId, Pageable pageable);
+}
